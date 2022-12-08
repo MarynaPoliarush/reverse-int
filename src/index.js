59a50eca9fbf84de72c87149daa8c59ef8a68660
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2022-12-08 15:37:28 +0200

    add:add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let reversedArray='';
+    let string = n.toString();
+   for (let i=string.length-1;i>=0; i--){
+      reversedArray=reversedArray+string[i]; 
+   }
+    return Number(reversedArray)
+    }
 }
