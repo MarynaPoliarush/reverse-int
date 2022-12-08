1a8acff50b6f920d5392acfdd948d7ff88898caf
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2022-12-08 15:39:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 module.exports = function reverse (n) {
-    let reversedArray='';
-    let string = n.toString();
-   for (let i=string.length-1;i>=0; i--){
-      reversedArray=reversedArray+string[i]; 
-   }
-    return Number(reversedArray)
-    }
+  
 }
